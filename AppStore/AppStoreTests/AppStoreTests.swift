81d0612a8a13bd6b784ea07a831f3a4ebf8195b6
angel zambrano <angeldzambrano99@gmail.com>
2024-12-06 14:05:39 -0800

    added default project

---
+++ AppStore/AppStoreTests/AppStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreTests.swift
+//  AppStoreTests
+//
+//  Created by angel zambrano on 12/6/24.
+//
+
+import Testing
+@testable import AppStore
+
+struct AppStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
